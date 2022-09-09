cd0e5c0ffab6ce5547d372547edc4afabee967ba
JosseMontano <josemariazambranamontano123@gmail.com>
2022-09-09 11:55:33 -0400

    bd

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,17 @@
 import { Pool } from "pg";
 const {db} = require('./config')
+
+/* local
 const pool = new Pool({
   user: db.user,
   password: db.password,
   host: db.host,
   port: db.port,
   database: db.database,
-});
+});*/
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+})
 
 module.exports = pool;
