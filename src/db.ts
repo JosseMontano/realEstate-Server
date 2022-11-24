51416add82577502b58250c5ae9c69f5ef0f26fb
JosseMontano <josemariazambranamontano123@gmail.com>
2022-11-24 10:35:09 -0400

    upload to github

--- src/db.ts
+++ src/db.ts
@@ -2,21 +2,21 @@ import { Pool } from "pg";
 import { DataSource } from "typeorm";
 const { methodDb1, methodDbTwo } = require("./config");
 
-/* method 1 to connect to postgres (dev)
+ //method 1 to connect to postgres (dev)
 const pool = new Pool({
   user: methodDb1.user,
-  password: methodDb1.password,
+  password: methodDb1.password ,
   host: methodDb1.host,
   port: methodDb1.port,
   database: methodDb1.database,
-});*/
+});
 
 //method 2 to connect to postgres (production)
-const pool = new Pool({
+/* const pool = new Pool({
   connectionString: methodDbTwo.databaseUrl,
   ssl: {
     rejectUnauthorized: false,
   },
-});
+}); */
 
 module.exports = pool;
