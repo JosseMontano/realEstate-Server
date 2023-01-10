4da01f7d1db5a7bda99ebe967c720ddcf8014c43
JosseMontano <josemariazambranamontano123@gmail.com>
2023-01-10 19:42:49 -0400

    doing build

--- src/db.ts
+++ src/db.ts
@@ -4,7 +4,7 @@ const { methodDb1, methodDbTwo } = require("./config");
  //method 1 to connect to postgres (dev)
 const pool = new Pool({
   user: methodDb1.user,
-  password: methodDb1.password ,
+  password: methodDb1.password,
   host: methodDb1.host,
   port: methodDb1.port,
   database: methodDb1.database,
